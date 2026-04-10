d4384d8304f8e4c90177cd22c46afabc7aff8fa5
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2026-04-10 13:10:37 +0200

    routes added to the main app

--- src/app.js
+++ src/app.js
@@ -11,20 +11,18 @@ setupMiddlewares(app);
 
 const PORT = process.env.PORT || 3000;
 
-// const accountsRouter = require('./routes/accounts');
-// const balanceSheetRouter = require('./routes/balanceSheet');
-// const transactionsRouter = require('./routes/transactions');
-// const trialBalanceRouter = require('./routes/trialBalance');
-//
-// app.use(`/api/v/${APIVersion}/accounts`, accountsRouter);
-// app.use(`/api/v/${APIVersion}/balance-sheet`, balanceSheetRouter);
-// app.use(`/api/v/${APIVersion}/transactions`, transactionsRouter);
-// app.use(`/api/v/${APIVersion}/trial-balance`, trialBalanceRouter);
-//
-// app.all('*', (req, res, next) => {
-//   return next(
-//     new AppError(`can't find ${req.originalUrl} on this server!`, 404),
-//   );
-// });
+const accountsRouter = require('./routes/accounts');
+const balanceSheetRouter = require('./routes/balanceSheet');
+const trialBalanceRouter = require('./routes/trialBalance');
+const transactionsRouter = require('./routes/transactions');
+
+app.use(`/api/v${APIVersion}/accounts`, accountsRouter);
+app.use(`/api/v${APIVersion}/balance-sheet`, balanceSheetRouter);
+app.use(`/api/v${APIVersion}/trial-balance`, trialBalanceRouter);
+app.use(`/api/v${APIVersion}/transactions`, transactionsRouter);
+
+const server = app.listen(PORT, () => {
+	console.log(`App running on port ${PORT}...`);
+});
 
 module.exports = app;
